9c1e6f505b278570fda9a0f9ca0627105e2b5402
DPfeyfer <Skydenfer737@gmail.com>
2023-01-08 14:19:06 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,51 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task 1
+        System.out.println("Task 1");
+        double deposit = 15000;
+        double totalDeposit = 0;
+        int totalMonth = 0;
+        while (totalDeposit < 2459000){
+            totalDeposit = totalDeposit + deposit;
+            totalMonth = totalMonth + 1;
+        }
+        System.out.println("we need " + totalMonth + " month for " + totalDeposit + "$");
+        System.out.println();
 
 
+        //task 2
+        System.out.println("Task 2");
+        System.out.println();
+
+
+        //task 3
+        System.out.println("Task 3");
+        System.out.println();
+
+
+        //task 4
+        System.out.println("Task 4");
+        System.out.println();
+
+
+        //task 5
+        System.out.println("Task 5");
+        System.out.println();
+
+
+        //task 6
+        System.out.println("Task 6");
+        System.out.println();
+
+
+        //task 7
+        System.out.println("Task 7");
+        System.out.println();
+
+
+        //task 8
+        System.out.println("Task 8");
+        System.out.println();
     }
 }
