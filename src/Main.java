ec327364ca3d30d9076bd091ccda15335eb531ef
DPfeyfer <Skydenfer737@gmail.com>
2023-01-11 16:01:01 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -57,18 +57,59 @@ public class Main {
         System.out.println();
 
 
+        //task 3.1
+        System.out.println("Task 3.1");
+        int numberOfPersons1 = 12_000_000;
+        int year = 0;
+        while (year < 10){
+            year ++;
+            numberOfPersons1 = numberOfPersons1 + growthOfPersons;
+            System.out.println(year + " year the number of persons is " + numberOfPersons1);
+        }
+        System.out.println();
+
+
         //task 4
         System.out.println("Task 4");
+        double deposit1 = 15000;
+        double percent = 7;
+        int month = 0;
+        while (deposit1 < 12_000_000){
+            deposit1 = deposit1 + (deposit1 * percent) / 100;
+            month ++;
+            System.out.println("The month " + month + " deposit will be " + String.format("%.2f",deposit1));
+        }
+
         System.out.println();
 
 
         //task 5
         System.out.println("Task 5");
+        double deposit2 = 15000;
+        double percent2 = 7;
+        int month2 = 0;
+        while (deposit2 < 12_000_000){
+            deposit2 = deposit2 + (deposit2 * percent2) / 100;
+            month2 ++;
+            if(month2 % 6 == 0){
+                System.out.println("The month " + month2 + " deposit will be " + String.format("%.2f",deposit2));
+            }
+        }
         System.out.println();
 
 
         //task 6
         System.out.println("Task 6");
+        double deposit3 = 15000;
+        double percent3 = 7;
+        int month3 = 0;
+        while (month3 < 108){
+            deposit3 = deposit3 + (deposit3 * percent3) / 100;
+            month3 ++;
+            if(month3 % 6 == 0){
+                System.out.println("The month " + month3 + " deposit will be " + String.format("%.2f",deposit3));
+            }
+        }
         System.out.println();
 
 
