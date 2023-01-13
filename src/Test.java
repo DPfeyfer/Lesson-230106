7152f4895f55c250664de9fc9beeee312f1099b7
DPfeyfer <Skydenfer737@gmail.com>
2023-01-13 15:29:12 +0300

    complete HW and test hello Den and Nat

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Denis");
-        System.out.println("hello Natali");
+        System.out.println("Hello Natali");
     }
 }
